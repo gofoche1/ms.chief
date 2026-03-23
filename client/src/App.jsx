cd497e46d18a577a3ee31c838def07d41499423c
SommieOgu <cynthiaogu04@gmail.com>
2026-03-23 13:27:29 -0400

    creating the react app with vite!

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
